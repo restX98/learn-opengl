1e6827e3c2b803b7a907c8e0cbb2449c39d54aed
Enrico Restuccia <enrico.rest98@gmail.com>
2024-07-08 23:07:54 +0200

    Coordinate system

---
+++ src/9_coordinate_system.cpp
@@ -0,0 +1,298 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <stb_image.h>
+
+#include <shader.hpp>
+
+#include <iostream>
+#include <cmath>
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+unsigned int createShaderProgram();
+
+const char* WINDOW_TITLE = "Two triangle";
+const unsigned int WINDOW_WIDTH = 800;
+const unsigned int WINDOW_HEIGHT = 600;
+
+// Very bad:
+float textureOpactity = 0.2f;
+
+int main() {
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+  GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, nullptr, nullptr);
+  if (window == NULL) {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
+  glEnable(GL_DEPTH_TEST);
+
+  Shader shader(SHADERS_PATH "9_shader.vert", SHADERS_PATH "9_shader.frag");
+
+  float vertices[] = {
+    // Positions          Texture coords
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+  };
+
+  int indices[] = {
+    0, 1, 2,
+    1, 2, 3
+  };
+
+  unsigned int VAO;
+  unsigned int VBO;
+  unsigned int EBO;
+
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
+
+  glBindVertexArray(VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+
+  // Texture:
+  unsigned int texture1;
+  glGenTextures(1, &texture1);
+
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_2D, texture1);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  int width, height, nrChannels;
+  unsigned char* data = stbi_load(RESOURCES_PATH "textures/container.png", &width, &height, &nrChannels, 0);
+  if (data) {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  } else {
+    std::cout << "Failed to load texture" << std::endl;
+  }
+  stbi_image_free(data);
+
+  unsigned int texture2;
+  glGenTextures(1, &texture2);
+
+  glActiveTexture(GL_TEXTURE1);
+  glBindTexture(GL_TEXTURE_2D, texture2);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  stbi_set_flip_vertically_on_load(true);
+  data = stbi_load(RESOURCES_PATH "textures/smile.png", &width, &height, &nrChannels, 0);
+  if (data) {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  } else {
+    std::cout << "Failed to load texture" << std::endl;
+  }
+  stbi_image_free(data);
+
+  shader.use();
+  shader.setInt("texture1", 0);
+  shader.setInt("texture2", 1);
+
+  // When we define the object vertex, we define it in a range (-1, 1).
+  // This coordinates need to be translated in Normalized Device Coordinates (NDC)
+  // These NDC are then given to the rasterizer to transform them to 2D coordinates/pixels on the screen.
+  // Transforming coordinates to NDC is usually accomplished in a step-by-step fashion where
+  // we transform an object's vertices to several coordinate systems before finally transforming them to NDC.
+  // There are a total of 5 different coordinate systems that are of importance to us:
+  //  - Local space (or Object space)
+  //  - World space
+  //  - View space (or Eye space)
+  //  - Clip space
+  //  - Screen space
+
+  //  Local space (or Object space):
+  // Local coordinates are the coordinates of your object relative to its local origin;
+  // they're the coordinates your object begins in.
+
+  // World space:
+  // The next step is to transform the local coordinates to world-space coordinates which are
+  // coordinates in respect of a larger world. These coordinates are relative to some global origin 
+  // of the world, together with many other objects also placed relative to this world's origin
+  // Done by a "Model Matrix"
+
+  // View space (or Eye space):
+  // Next we transform the world coordinates to view-space coordinates in such a way that each
+  // coordinate is as seen from the camera or viewer's point of view.
+  // Done by a "View Matrix"
+
+  // Clip space:
+  // After the coordinates are in view space we want to project them to clip coordinates.
+  // Clip coordinates are processed to the -1.0 and 1.0 range and determine which vertices will end
+  // up on the screen. Projection to clip-space coordinates can add perspective if using perspective projection.
+  // Done by "Projection Matrix"
+
+  // Screen space:
+  // And lastly we transform the clip coordinates to screen coordinates in a process we call viewport transform
+  // that transforms the coordinates from -1.0 and 1.0 to the coordinate range defined by glViewport.
+  // The resulting coordinates are then sent to the rasterizer to turn them into fragments.
+  // Done by "Viewport Transform"
+
+  glm::vec3 cubePositions[] = {
+    glm::vec3(0.0f,  0.0f,  0.0f),
+    glm::vec3(1.5f,  0.2f, -1.5f),
+    glm::vec3(2.0f,  5.0f, -15.0f),
+    glm::vec3(-1.5f, -2.2f, -2.5f),
+    glm::vec3(-3.8f, -2.0f, -12.3f),
+    glm::vec3(2.4f, -0.4f, -3.5f),
+    glm::vec3(-1.7f,  3.0f, -7.5f),
+    glm::vec3(1.3f, -2.0f, -2.5f),
+    glm::vec3(1.5f,  2.0f, -2.5f),
+    glm::vec3(-1.3f,  1.0f, -1.5f)
+  };
+
+  // The view matrix move the entire scene around inversed to where we want the camera to move.
+  // In this case, we want to move backwars the camera (from our perpispective, but considering 
+  // OpenGL is a right-handed system [right hand rules YOU KNOW IT!] we should move the camera
+  // forward), then we need to move the scene/object forward (backward for rh system), then we
+  // move it backward in the z-axis of 3 point.
+  glm::mat4 view = glm::mat4(1.0f); // Create 4x4 identity matrix
+  // Create a translation matrix that move the container backward of 3 on the z-axis
+  view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+
+  // Then projection matix is used to define:
+  // - fov (field of view): set how large the viewspace is (usually 45 deg)
+  // - aspect ration: width/height
+  // - near plane
+  // - far plane
+  glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
+  shader.setMat4("view", view);
+  shader.setMat4("projection", projection);
+
+  while (!glfwWindowShouldClose(window)) {
+    processInput(window);
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    shader.setFloat("opacity", textureOpactity);
+
+    for (int i = 0; i < 10; i++) {
+      // The model matrix consists of translations, scaling and/or rotation we want to apply
+      // to transform/move the object mappend in the Local Space to the world space (done also in the transformation chapter)
+      glm::mat4 model = glm::mat4(1.0f); // Create 4x4 identity matrix
+      // Create rotation matrix fixing the x-axis
+      model = glm::translate(model, cubePositions[i]);
+      float angle = 20.0f * (i + 1);
+      model = glm::rotate(model, (float)glfwGetTime() * glm::radians(angle), glm::vec3(0.5f, 1.0f, 0.0f));
+
+      shader.setMat4("model", model);
+
+      glBindVertexArray(VAO);
+      glDrawArrays(GL_TRIANGLES, 0, 36);
+    }
+
+    // Order is important when multiplying matrices, use the following order:
+    // 1. scaling operations
+    // 2. rotations operations
+    // 3. translations operations
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+  glDeleteBuffers(1, &EBO);
+
+  glfwTerminate();
+  return 0;
+}
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  } else if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+    textureOpactity += 0.001f;
+    if (textureOpactity > 1) {
+      textureOpactity = 1.0f;
+    }
+  } else if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+    textureOpactity -= 0.001f;
+    if (textureOpactity < 0) {
+      textureOpactity = 0.0f;
+    }
+  }
+}
